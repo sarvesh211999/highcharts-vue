9584087aac530060dbf468c62b9277f4e03d7c3d
sarvesh211999 <sarvesh211999@gmail.com>
2018-06-26 03:59:03 +0530

    watch example

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,32 @@
 <template>
   <div>
-    <one :options="options"></one>
-    <two :options="options"></two>
-
+  	{{ a }}
   </div>
 </template>
 
 <script>
-import one from 'vue2-highcharts'
-import two from 'vue2-highcharts'
 
-const asyncData = {
-  name: 'Tokyo',
-  marker: {
-    symbol: 'square'
-  },
-  data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
-    y: 26.5,
-    marker: {
-      symbol: 'url(http://www.highcharts.com/demo/gfx/sun.png)'
-    }
-  }, 23.3, 18.3, 13.9, 9.6]
-}
 export default{
-    components: {
-        one,
-        two
-    },
-    data(){
+    data: function(){
       return{
-        options: {
-          chart: {
-            type: 'spline'
-          },
-          title: {
-            text: 'Monthly Average Temperature'
-          },
-          subtitle: {
-            text: 'Source: WorldClimate.com'
-          },
-          xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-          },
-          yAxis: {
-            title: {
-              text: 'Temperature'
-            },
-          },
-          series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
-          }, {
-            name: 'John',
-            data: [1 ,1 ,1, 1 ,1 ,1, 1 ,1 ,3, 1 ,2 ,3]
-          }]
-        }
+        a:1,
       }
     },
+    mounted: function(){
+    	this.start();
+    },
+    methods:{
+    	start: function(){
+    		this.a =  this.a + 1;
+    		setTimeout(this.start,1000);
+    	}
+    },
+    watch:{
+    	a: function(){
+    		console.log(this.a);
+    	}
+    }
+
 
 };
 </script>
